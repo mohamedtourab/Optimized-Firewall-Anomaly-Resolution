0a07d004ffd54eb058bbec786ea53796de0443dd
MohamedMamdouh95 <mohamedmtourab@gmail.com>
2020-03-31 23:23:53 +0300

    Removing comment

--- src/data/serializer/DataMarshaller.java
+++ src/data/serializer/DataMarshaller.java
@@ -25,7 +25,6 @@ public class DataMarshaller {
             File file = new File(outputFilePath);
             m.marshal(data, file);
         } catch (JAXBException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
 }
