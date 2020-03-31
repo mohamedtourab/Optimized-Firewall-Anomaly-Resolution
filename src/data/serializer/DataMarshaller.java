b5fada8f9a74ed3436e10791fdce5b4870a265c8
MohamedMamdouh95 <mohamedmtourab@gmail.com>
2020-03-31 16:31:27 +0300

    Adding Data marshaller

---
+++ src/data/serializer/DataMarshaller.java
@@ -0,0 +1,43 @@
+package data.serializer;
+
+
+import ofar.generated.classes.conflicts.Anomalies;
+import ofar.generated.classes.input.ServiceInput;
+import ofar.generated.classes.rules.Rules;
+import optimized.resolution.algorithm.classes.DataCreator;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.File;
+
+public class DataMarshaller {
+
+    public static void marshalData(Object data,String contextPath,String outputFilePath){
+        // create a Marshaller and marshal to a file
+        try {
+            //contextPath = "ofar.generated.classes.input"
+            JAXBContext jc = JAXBContext.newInstance(contextPath);
+            Marshaller m = jc.createMarshaller();
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            //"D:\\MohamedMamdouh\\Education\\PoliTo\\Projects\\Optimized-Firewall-Anomaly-Resolution\\src\\data\\serializer\\serviceInput.xml"
+            // Store XML to File
+            File file = new File(outputFilePath);
+            m.marshal(data, file);
+        } catch (JAXBException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+}
+
+    public static void main(String[] args) {
+        //create the data to be serialized
+        Rules rules = DataCreator.createRules();
+        Anomalies anomalies = DataCreator.createAnomalies();
+        ServiceInput serviceInput = new ServiceInput();
+        serviceInput.setAnomaliesList(anomalies);
+        serviceInput.setDefectedRules(rules);
+        String outputPath = "D:\\MohamedMamdouh\\Education\\PoliTo\\Projects\\Optimized-Firewall-Anomaly-Resolution\\src\\data\\serializer\\serviceInput.xml";
+        marshalData(serviceInput,"ofar.generated.classes.input",outputPath);
+    }
+}
